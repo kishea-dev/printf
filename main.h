c2282ee725becd0235c9a4d18919cab19d028bcb
Moses Kisya <kishea.dev@gmail.com>
2023-01-24 21:18:41 +0300

    firat commit

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int
+ */
+int _printf(char *format, ...);
+#endif

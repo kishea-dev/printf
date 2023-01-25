678d28bb781e1e10dbd50c19fbd7877bf4e06a43
Moses Kisya <kishea.dev@gmail.com>
2023-01-25 16:48:45 +0300

    added printinf and wrinting code.
    print hexadecimals
    print strings
    print numbers
    print pointers
    print unsigned
    print reverse
    print percent
    print octal
    print chars
    print non-printables

--- get_flags.c
+++ get_flags.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
@@ -12,7 +13,7 @@ int get_flags(const char *format, int *i)
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
-	
+
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
@@ -25,8 +26,8 @@ int get_flags(const char *format, int *i)
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
-	
+
 	*i = curr_i - 1;
-	
+
 	return (flags);
 }
